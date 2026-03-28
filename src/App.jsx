b3dbb9e780c31c8a34f7b4cdfef07e48212e1023
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-03-28 21:12:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Test from "./Components/Test";
+
+function App() {
+  return (
+    <>
+      <Test id="1" />
+  
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Test.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Test = ({ id }) => {
+    const handleClick = (para) => {
+        alert(`Button in Test component with ID ${Number(id) + Number(para)} clicked!`);
+    }
+    const [count, setCount] = useState(0);
+
+    const handleClick2 = ()=>{
+        setCount(count + 1);
+        console.log(`Count: ${count}`);
+    }
+
+  return (
+    <>
+      <div>
+        <h1>Test Component</h1>
+        <p>ID: {id}</p>
+        <p>{`Count: ${count < 10 ? `${count} - Equal or less than ten!` : `${count} - more than ten!`}`}</p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse illo
+          eligendi quaerat consequatur at recusandae blanditiis tempora
+          provident officia, maiores quod veniam asperiores fugit, voluptate,
+          suscipit magnam cupiditate architecto molestias. Atque accusantium
+          assumenda, iusto totam laudantium tenetur ab numquam maiores
+          perspiciatis excepturi expedita beatae?
+        </p>
+      </div>
+      <button onClick={()=> handleClick(20)}>Click Me</button>
+      <button onClick={handleClick2}>Click Me 2</button>
+    </>
+  );
+};
+
+export default Test;
